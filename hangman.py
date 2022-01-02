aef0d3e83d898ba361fe34efbcda01c8bce6b55b
Michael Garcia <garciamichael.developer@gmail.com>
2022-01-02 13:01:51 -0700

    method to get valid word from words w/o "-" or " "

--- hangman.py
+++ hangman.py
@@ -1,3 +1,10 @@
 import random
 from words import words
 
+def get_valid_words(words):
+    word = random.choice(words) #takes in list and chooses from list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+        
+    return word
+    
