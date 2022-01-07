ebd0ca270373f5af2b032d05f9236b5857389cfb
Michael Garcia <garciamichael.developer@gmail.com>
2022-01-06 21:02:17 -0700

    prints lives with unicode. Deducts hearts

--- hangman.py
+++ hangman.py
@@ -1,7 +1,6 @@
 import random
 from words import words
 import string
-
 def get_valid_words(words):
     word = random.choice(words) #takes in list and chooses from list
     while '-' in word or ' ' in word:
@@ -15,11 +14,16 @@ def hangman():
     alphabet = set(string.ascii_uppercase) # letters in the alphabet
     used_letters = set() # what letters the user has guessed
     
+    lives = 6
     #getting user input
     #loop until word is complete: set word_letters length is zero
     while len(word_letters) > 0:
         #letters used
-        print('You have used these letters: ', ' '.join(used_letters))
+        print('Lives: ', end=" ")
+        for x in range(lives): 
+            print ('\u2764',end=" ")
+        print('\nYou have used these letters: ', ' '.join(used_letters))
+        
         #what the current word is
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current word: ', ' '.join(word_list))
@@ -30,6 +34,8 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                lives -= 1 #Takes away a life if wrong
         #User input letter already used
         elif user_letter in used_letters:
             print("Character already used. Please try again!")
